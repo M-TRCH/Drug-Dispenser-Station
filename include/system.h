3ba5fe5ad30bfd7a10d500cd1df387436f26daa0
Papadampk <papada.mpk@gmail.com>
2025-10-03 15:56:15 +0700

    PINS edited

--- include/system.h
+++ include/system.h
@@ -6,51 +6,54 @@
 #include <HardwareSerial.h>
 #include <ArduinoRS485.h>
 
-// Pin definitions for the STM32G431CBU6 board
-#define LED_RUN_PIN     PC10
-#define LED_CAL_PIN     PC11
-#define LED_ERR_PIN     PC13
-#define SW_STOP_PIN     PA0
+// Pin definitions for the STM32G431CBU6 board == 20 PINS edited
+#define LED_STATUS_PIN  PC13
+#define SW_CALC_PIN     PA0
 #define SW_START_PIN    PA1
 #define PWM_A_PIN       PB0
 #define PWM_B_PIN       PB1
 #define PWM_C_PIN       PB13
+#define SCK_PIN         PA5
 #define MISO_PIN        PA6 
 #define MOSI_PIN        PA7   
-#define SCK_PIN         PA5
 #define CS_ENC_PIN      PB12
-#define ADC_IA_PIN      PA2
-#define ADC_IC_PIN      PA3
-#define ADC_VDC_PIN     PA4
-#define SEN_1_PIN       PA15
-#define SEN_2_PIN       PB9
-#define RX_PIN          PB11  // RX pin for Serial2
-#define TX_PIN          PB10  // TX pin for Serial2
-#define RX3_PIN         PA10
-#define TX3_PIN         PA9
+#define SENSE_A_PIN     PA2
+#define SENSE_C_PIN     PA3
+#define SEN_1_PIN       PB9
+#define SEN_2_PIN       PA15
+#define RS485_RX3_PIN   PB11    // RS485 RX3
+#define RS485_TX3_PIN   PB10    // RS485 TX3
+#define RS232_RX1_PIN   PB7     // RS232 RX1
+#define RS232_TX1_PIN   PC4     // RS232 TX1
+#define SYS_RX1_PIN     PA9     // SYS RX1
+#define SYS_TX1_PIN     PA10    // SYS TX1
 
-// (1) System constants
-#define SERIAL3_BAUDRATE         2000000U
+// System constants
+#define SERIAL_BAUDRATE          9600U
+#define SERIAL_TIMEOUT           500U
+#define SERIAL_DECIMAL_PLACES    2
+#define SERIAL3_BAUDRATE         9600U
 #define SERIAL3_TIMEOUT          500U
 #define SERIAL3_DECIMAL_PLACES   2
+
 #define ANALOG_READ_RESOLUTION   12U
 #define MY_PWM_FREQUENCY         20000U
 #define MY_PWM_RESOLUTION        12U
 
-// (2) Object definitions
-extern HardwareSerial Serial2;   // RX, TX pins for Serial2
-extern HardwareSerial Serial3;   // RX3, TX3 pins for Serial3 (debugging)
+// Object definitions
+extern HardwareSerial Serial;  
+extern HardwareSerial Serial3;   
 extern RS485Class rs485;
 
-// (3) Macros definitions
+// Macros definitions
 #define SW_START_PRESSING  (digitalRead(SW_START_PIN) == LOW)
-#define SW_STOP_PRESSING   (digitalRead(SW_STOP_PIN) == LOW)
+#define SW_CALC_PRESSING   (digitalRead(SW_CALC_PIN) == LOW)
 #define SENSOR_1_ACTIVE    (digitalRead(SEN_1_PIN) == LOW)
 #define SENSOR_2_ACTIVE    (digitalRead(SEN_2_PIN) == LOW)
 
-// (5) Communication settings
-#define DEBUG_Baud      9600
-#define MODBUS_Baud     9600
+// Communication settings
+#define DEBUG_BAUD      9600
+#define MODBUS_BAUD     9600
 
 /* @brief System initialization function
  * 
--- src/system.cpp
+++ src/system.cpp
@@ -1,38 +1,38 @@
 
 #include "system.h"
 
-HardwareSerial Serial2(RX_PIN, TX_PIN);           // RX, TX pins for Serial2
-HardwareSerial Serial3(RX3_PIN, TX3_PIN);         // RX3, TX3 pins for Serial3 (debugging)
-RS485Class rs485(Serial2, PA7, TX_PIN, RX_PIN);
+//HardwareSerial Serial2(RX_PIN, TX_PIN);           // RX, TX pins for Serial2
+//HardwareSerial Serial3(RX3_PIN, TX3_PIN);         // RX3, TX3 pins for Serial3 (debugging)
+//RS485Class rs485(Serial2, PA7, TX_PIN, RX_PIN);
 
 void systemInit() 
 {
     // Serial2 for RS485 communication
-    Serial2.begin(MODBUS_Baud, SERIAL_8N1);
+    //Serial2.begin(MODBUS_Baud, SERIAL_8N1);
 
     // Serial3 for debugging
-    Serial3.begin(SERIAL3_BAUDRATE);
-    Serial3.setTimeout(SERIAL3_TIMEOUT);
+    //Serial3.begin(SERIAL3_BAUDRATE);
+    //Serial3.setTimeout(SERIAL3_TIMEOUT);
 
     // Initialize pins
-    pinMode(LED_RUN_PIN, OUTPUT);
-    pinMode(LED_CAL_PIN, OUTPUT);
-    pinMode(LED_ERR_PIN, OUTPUT); 
-    pinMode(SW_STOP_PIN, INPUT);
-    pinMode(SW_START_PIN, INPUT);
-    pinMode(SEN_1_PIN, INPUT);
-    pinMode(SEN_2_PIN, INPUT);
+    //pinMode(LED_RUN_PIN, OUTPUT);
+    //pinMode(LED_CAL_PIN, OUTPUT);
+    //pinMode(LED_ERR_PIN, OUTPUT); 
+   // pinMode(SW_STOP_PIN, INPUT);
+   // pinMode(SW_START_PIN, INPUT);
+    //pinMode(SEN_1_PIN, INPUT);
+    //pinMode(SEN_2_PIN, INPUT);
 
-    analogReadResolution(ANALOG_READ_RESOLUTION); // Set ADC resolution to 12 bits for current sensing
+    //analogReadResolution(ANALOG_READ_RESOLUTION); // Set ADC resolution to 12 bits for current sensing
 
-    analogWriteFrequency(MY_PWM_FREQUENCY);
-    analogWriteResolution(MY_PWM_RESOLUTION);  // Set PWM resolution to 12 bits for SVPWM
+    //analogWriteFrequency(MY_PWM_FREQUENCY);
+   // analogWriteResolution(MY_PWM_RESOLUTION);  // Set PWM resolution to 12 bits for SVPWM
 }
 
 void setLEDBuiltIn(bool run, bool cal, bool err, int delay_time)
 {
-    digitalWrite(LED_RUN_PIN, run);
-    digitalWrite(LED_CAL_PIN, cal);
-    digitalWrite(LED_ERR_PIN, err);
-    delay(abs(delay_time));
+    //digitalWrite(LED_RUN_PIN, run);
+    //digitalWrite(LED_CAL_PIN, cal);
+    //digitalWrite(LED_ERR_PIN, err);
+   // delay(abs(delay_time));
 }
